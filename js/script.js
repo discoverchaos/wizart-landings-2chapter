6c407803f8b0a06f3fb06e1f82b643159faea6d9
nur <nursultanallabergenov16@gmail.com>
2022-09-25 11:50:53 +0500

    new-project-2chapter

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('.chevron__with__title').click(function (event) {
+        $(this).toggleClass('active').next().slideToggle(300);
+    })
+})
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: swiper,
+    },
+});
